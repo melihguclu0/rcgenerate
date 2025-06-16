ba0647bcba954db635f2ec003e51c8b2bd13fdef
melih <s24ial34@proton.me>
2025-06-16 21:35:43 +0300

    Big fixes

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 # rcgenerate
 
 Simple script to generate KDEConnect Run Command configuration file,
-particularly helpful for Raspberry Pi setups without a display
-
+particularly helpful for Raspberry Pi setups without a display. Using this script, you can add new commands and synchronize across all connected devices easily. 
 
 ## Requirements:
+
 `jq` Command-line JSON processor
+
 ### Usage:
 
-1.  `chmod +x rcgenerate`
-2.  `./rcgenerate -r` Create initial configuration
-3.  `./rcgenerate -n command_name command_path` Add new commands or edit "commands.json" file manually.
-4.  `./rcgenerate -i` Generate and install configuration file
-5.  Commands will appear shortly in other devices. Running `kdeconnect-cli --refresh` might speed up the process. 
+1.  `chmod +x ./rcgenerate.sh`
+2.  `./rcgenerate.sh -r` Reset and create the initial configuration
+3.  `./rcgenerate.sh -g` Get existing configuration file
+4.  `./rcgenerate.sh -n "command_name" "command_path"` Add new commands or edit
+    "commands_${ID}.json" file manually.
+5. `./rcgenerate.sh -s` Set existing configuration file
+6.  As with the new random ID's assigned for each command - they will take a while to appear on the other devices. Running `kdeconnect-cli
+    \--refresh` might speed up the process. On Android **Settings > Apps > KDEConnect > Force Close** then restart.
 
+### Limitations:
+
+- For now, its not possible to assign commands for individual device. New commands added globally.
 
 ### Known Problems:
 
-Double quotes are problematic. Single quotes seem to work but probably makes variable expansion, command substitution etc. impossible.
+Double quotes are problematic. Single quotes seem to work but probably makes
+variable expansion, command substitution etc. impossible.
+
--- rcgenerate.sh
+++ rcgenerate.sh
@@ -3,18 +3,20 @@ dash_help()
 {
    echo
    echo "RCGenerate"
-   echo "version 0.1"
+   echo "version 0.5"
    echo
    echo "This simple script is designed to assist in creating a KDEConnect \"Run Command\" configuration file, particularly helpful for Raspberry Pi setups without a monitor." 
-   echo "Warning! Quotes need fixing in v0.1"
-   echo "Use single quotes if you need enclosing something. Otherwise commands wont appear in other devices."
+   echo "Warning! Quotes need fixing"
+   echo "Use single quotes if you need enclosing something. Otherwise commands may not appear in other devices."
    echo
    echo "Usage: rcgenerate [options] name path"
    echo "options:"
-   echo "  -n, --new       Add a new command"
-   echo "  -i, --install   Generate and install configuration file"
-   echo "  -r, --reset     Reset and install configuration file"
-   echo "  -h, --help      Print this help"
+   echo "  -g, --get-current    Get current commands"
+   echo "  -s, --set-current    Set current commands"
+   echo "  -n, --add-command    Add a new command"
+   echo "  -l, --list-devices   List remote devices"
+   echo "  -r, --reset          Reset configuration"
+   echo "  -h, --help           Print this help"
    echo
    echo "Examples:"
    echo "  rcgenerate -n name path    Create a command with given name and path"
@@ -24,6 +26,14 @@ dash_help()
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+get_dev() {
+
+while IFS= read -r line; do
+    array+=("$line")
+done < <(kdeconnect-cli -l | grep -wEo "_?[a-zA-Z0-9]{8}_[a-zA-Z0-9]{4}_[a-zA-Z0-9]{4}_[a-zA-Z0-9]{4}_[a-zA-Z0-9]{12}_?")
+    echo ${array[@]}
+}
+
 gen_random(){
     random=$(openssl rand -hex 16)  
     echo "$random" | sed -E 's/(.{8})(.{4})(.{4})(.{4})(.{12})/\1_\2_\3_\4_\5/'
@@ -32,7 +42,14 @@ gen_random(){
 new_command() {
 random=$(gen_random)
 
-old_data=$(cat ${SCRIPT_DIR}/commands.json)
+read -r -a DEV <<< "$(get_dev)"
+echo ${DEV[@]}
+
+command=$(echo $command | sed 's/"/\\\"/g')
+path=$(echo $path | sed 's/"/\\\"/g')
+for i in "${DEV[@]}"; do
+old_data=$(cat ${SCRIPT_DIR}/commands_$i.json)
+
 new_data=$(cat <<EOF
     {
     "_${random}_": {
@@ -43,61 +60,87 @@ new_data=$(cat <<EOF
 EOF
 )
 
-final_data=$(jq -n --argjson a "$new_data" --argjson b "$old_data" '$a + $b')
-echo $final_data > ${SCRIPT_DIR}/commands.json
+data=$(jq -n --argjson a "$new_data" --argjson b "$old_data" '$a + $b')
+echo $data > ${SCRIPT_DIR}/commands_$i.json
+done
 echo
 echo "name:$command command:$path"
-echo "New command added. Now run with -i to generate and install config file"
+echo "New command added. Now run with \"-s\" to install configuration file"
 echo
 exit 0
 }
 
-rst() 
+reset()
 {
-read -p "Warning! This will overwrite your existing configuration. Proceed? Y/N" de
-echo 
+
+echo "Resetting..."
+random=$(gen_random)
+
+read -r -a DEV <<< "$(get_dev)"
+
+for i in "${DEV[@]}"; do
+rm ${SCRIPT_DIR}/commands_$i.json 2> /dev/null
+cat <<EOF > ${SCRIPT_DIR}/commands_$i.json
+{
+"_${random}_": {
+    "command": "notify-send Ping!",
+    "name": "Ping Device"
+    }
+}
+EOF
+done
+
+    echo "Done"
+
+}
+
+
+read_current() {
+
+
+    read -r -a DEV <<< "$(get_dev)"
+
+for i in "${!DEV[@]}"; do
+    config_file="${HOME}/.config/kdeconnect/${DEV[$i]}/kdeconnect_runcommand/config"
+
+    escaped_json=$(grep '^commands="@ByteArray' "$config_file" | sed -E 's/^commands="@ByteArray\((.*)\)"$/\1/')
+
+    cleaned_json=$(echo "$escaped_json" | sed 's/\\"/"/g')
+
+    echo "$cleaned_json" | jq . > "./commands_${DEV[$i]}.json"
+
+done
+
+
+    exit 23
+}
+
+install() {
+    read -p "Warning! This will overwrite your existing configuration. Proceed? Y/N" de
+echo
 
 if [[ $de = "y" || $de = "Y" ]]; then
 
     :
+
 elif [[ $de = "n" || $de = "N" ]]; then
+
     echo "Cancel"
     exit 1
+
 else
+
     echo "Cancel"
     exit 1
 fi
 
-echo "Config Reset..."
-random=$(gen_random)
-#fix this double quotes
-cat <<EOF > ${SCRIPT_DIR}/commands.json
-{
-"_${random}_": {
-    "command": "echo 'Echo' >> /dev/pts/$(ps | grep -o 'pts/.' | cut -d'/' -f2 | head -n1)",
-    "name": "echo"
-    }
-}
-EOF
-
-    generate
-    echo "Done"
-    exit 0
-}
-
-generate() {
-    
     echo "Generating configuration file..."
-    data=$(cat ${SCRIPT_DIR}/commands.json)
-    h=$(echo $data | sed 's/"/\\"/g')
+    read -r -a DEV <<< "$(get_dev)"
 
-    while IFS= read -r line; do
-        array+=("$line")
-    done < <(kdeconnect-cli -l | grep -wEo "_?[a-zA-Z0-9]{8}_[a-zA-Z0-9]{4}_[a-zA-Z0-9]{4}_[a-zA-Z0-9]{4}_[a-zA-Z0-9]{12}_?")
-
-
-    for element in "${array[@]}"; do
-        echo -e "[General]\ncommands=\"@ByteArray(${h})\"" > "${HOME}/.config/kdeconnect/${element}/kdeconnect_runcommand/config"
+    for i in "${DEV[@]}"; do
+        data=$(cat ${SCRIPT_DIR}/commands_$i.json)
+        h=$(echo $data | sed 's/"/\\\"/g')
+        echo -e "[General]\ncommands=\"@ByteArray(${h})\"" > "${HOME}/.config/kdeconnect/$i/kdeconnect_runcommand/config"
     done
     echo "Done"
     exit 0
@@ -113,47 +156,65 @@ if [[ $# > 3 ]]; then
     exit 2
 
 else
-    
    : #Do nothing and proceed
 fi
 
 if [[ $1 = "-r"  || $1 = "--reset" ]]; then
+    
+    reset
+    
+elif [[ $1 = "-g" || $1 = "--get-current" ]]; then
 
-    rst
+    read_current
 
-elif [[ $1 = "-n" || $1 = "--new" ]]; then
+elif [[ $1 = "-n" || $1 = "--add-new" ]]; then
     
 
-    if [[ -f ${SCRIPT_DIR}/commands.json ]]; then
+read -r -a DEV <<< "$(get_dev)"
+
+for i in "${DEV[@]}"; do
+    file="${SCRIPT_DIR}/commands_${i}.json"
+
+    if [[ -f "$file" ]]; then
         :
     else
-        echo "No config file found"
-        echo "Creating..."
-        rst
+        echo "Missing configuration files. Run with \"-r\""
+        exit 1
     fi
-    
+done
+
+
+
     if [[ -n $2 ]] && [[ -n $3 ]]; then
-        # If both $2 and $3 are not empty, use them
+
         command=$2
         path=$3
         
         new_command "$command" "$path"
     else
-        # Otherwise, prompt the user for input
+
         read -p "Command name: " command
         read -p "Command path: " path
         new_command "$command" "$path"
     fi
     exit 0
     
-elif [[ $1 = "-i" || $1 = "--install" ]]; then
+elif [[ $1 = "-s" || $1 = "--set-current" ]]; then
     
-    generate
+    install
     
 elif [[ $1 = "" || $1 = "--help" ]]; then
     
     dash_help
     exit 0
+
+elif [[ $1 = "-l" || "--list-devices" ]]; then
+
+    read -r -a DEV <<< "$(get_dev)"
+    for i in "${!DEV[@]}"; do
+        echo "Device$i ID: ${DEV[$i]}"
+    done
+
 else
     echo
     echo "Argument unknown. Try --help"
